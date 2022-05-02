bfaa87dd6876fe4f56a14cef25b78b02de8f9904
陈金伙 <chenjinhuo@bytedance.com>
2022-05-03 06:24:54 +0800

    chore: add eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  proseWrap: 'never',
+  bracketSpacing: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  htmlWhitespaceSensitivity: 'ignore'
+}
