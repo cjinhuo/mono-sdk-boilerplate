e4a5b5873746086c14a4d4a687de6392e4405c7a
陈金伙 <chenjinhuo@bytedance.com>
2023-02-11 15:22:03 +0800

    feat(eslint): add tsconfigRootDir for each package eslint

--- .eslintrc.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  root: true,
-  extends: '@mono/eslint-config',
-}
-module.exports = config
---
+++ packages/react/.eslintrc.cjs
@@ -0,0 +1,9 @@
+const config = {
+  root: true,
+  extends: '@mono/eslint-config',
+  parserOptions: {
+    // 可优化至 eslint-config 中，动态获取 tsconfigRootDir 路径
+    tsconfigRootDir: __dirname,
+  },
+}
+module.exports = config
