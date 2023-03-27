b41752e46edf4fb0f3b06d73041db2ae72a08145
陈金伙 <chenjinhuo@bytedance.com>
2023-03-27 13:17:31 +0800

    test(e2e): :white_check_mark: complete the e2e coverage

--- e2e/demos/react-e2e-demo/src/components/TestCore.tsx
+++ e2e/demos/react-e2e-demo/src/components/TestCore.tsx
@@ -1,16 +1,27 @@
 import { useState } from 'react'
 
-import { coreAdd } from '@mono/core'
+import { coreAdd, coreIsString } from '@mono/core'
 
 export default function TestCore() {
   const [count, setCount] = useState(0)
-
+  const [str] = useState('str')
+  const [num] = useState(-1)
   return (
     <div>
-      <button id='test-core-button' onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      {count} + {count} = <span id='test-core-result'>{coreAdd(count, count)}</span>
+      <div>
+        <button id='test-core-button' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        {count} + {count} = <span id='test-core-result'>{coreAdd(count, count)}</span>
+      </div>
+      <div id='test-core-string'>
+        {str} is
+        {coreIsString(str) ? ' string' : ' non-string'}
+      </div>
+      <div id='test-core-non-string'>
+        {num} is
+        {coreIsString(num) ? ' string' : ' non-string'}
+      </div>
     </div>
   )
 }
